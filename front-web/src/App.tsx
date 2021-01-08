e169692a8ed906a99744fabde65aeb16dac933c4
Rônei Társis <roneitarsissilvadaltro@gmail.com>
2021-01-08 09:32:53 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+import Navbar from './Navbar';
 
 
 
@@ -9,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
