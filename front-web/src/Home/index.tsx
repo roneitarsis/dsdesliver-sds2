efe5e7d1a8adf5e0ebd515fef8548ce4de1e78f1
Rônei Társis <roneitarsissilvadaltro@gmail.com>
2021-01-08 16:42:31 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,31 +1,37 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
-import { ReactComponent as MainImage} from './main.svg';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
-function Home(){
+function Home() {
 
-    return(
+    return (
 
-        <div className="home-container">
-               <div className="home-content">
-                   <div className="home-actions">
-                     <h1 className="home-title">
-                        Faça seu pedido <br /> que entregam <br />pra você !!!!   
-                     </h1>  
-                     <h3 className="home-subtit">
-                     Escolha seu pedido e em poucos minutos<br />
-                     levaremos na sua porta   </h3> 
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br /> que entregam <br />pra você !!!!
+                     </h1>
+                        <h3 className="home-subtit">
+                            Escolha seu pedido e em poucos minutos<br />
+                     levaremos na sua porta   </h3>
 
-                     <a href="orders" className="home-btn-order">
-                        Fazer pedido  
-                      </a>  
+                        <a href="orders" className="home-btn-order">
+                            Fazer pedido
+                      </a>
 
-                   </div> 
-                   <div className="home-image">
-                       <MainImage />
-                    </div>                  
-              </div> 
-        </div>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
+                </div>
+            </div>
+
+           <Footer /> 
+
+        </>
     )
 
 }
